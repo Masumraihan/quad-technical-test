5d6ccd6e52534e488b1445665be693574cb9619c
Masumraihan <mdmasumraihan1@gamil.com>
2024-01-07 08:55:21 +0600

    refactor:add https in api

--- src/api/utils.js
+++ src/api/utils.js
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 export const getProductsData = async () => {
   try {
     const res = await fetch(
-      "http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10",
+      "https://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10",
     );
     const result = await res.json();
     return await result;
