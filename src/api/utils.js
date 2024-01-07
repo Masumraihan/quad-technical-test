618395c051f728a4d597eb7589e4a2afdf33e104
Masumraihan <mdmasumraihan1@gamil.com>
2024-01-07 09:05:36 +0600

    refactor:remove https from api

--- src/api/utils.js
+++ src/api/utils.js
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 export const getProductsData = async () => {
   try {
     const res = await fetch(
-      "https://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10",
+      "http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10",
     );
     const result = await res.json();
     return await result;
