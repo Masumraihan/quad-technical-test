140d67f05e6ddff27c136a0407c94be469f54646
Masumraihan <mdmasumraihan1@gamil.com>
2024-01-07 09:20:07 +0600

    add:use axios for data fetching

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,12 +1,13 @@
+import axios from "axios";
 import { toast } from "sonner";
 
 export const getProductsData = async () => {
   try {
-    const res = await fetch(
+    const res = await axios.get(
       "http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10",
     );
-    const result = await res.json();
-    return await result;
+    //const result = await res.json();
+    return await res.data;
   } catch (error) {
     toast.error("Failed to fetch products");
   }
